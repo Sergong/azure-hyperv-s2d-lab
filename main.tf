f9b1feeef54971761f5b9c01e5d70a71f3f9868a
serge.meeuwsen@gmail.com <serge.meeuwsen@gmail.com>
2025-08-01 11:47:45 +0100

    Updated files and gitignore

--- main.tf
+++ main.tf
@@ -1,6 +1,42 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.11"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
+
+# Get the current public IP of the machine running Terraform
+data "http" "current_ip" {
+  url = "https://ipv4.icanhazip.com"
+}
+
+locals {
+  current_ip = chomp(data.http.current_ip.response_body)
+}
+
+// Ensure you export TF_VAR_admin_password before running this!
+variable "admin_password" {
+  description = "Admin password for Windows VM"
+  type        = string
+  sensitive   = true
+}
+
+variable "admin_username" {
+  description = "Admin username for the Windows VM"
+  type        = string
+  default     = "adm-smeeuwsen"  # Optional: set a default or omit this if it must be provided explicitly
+}
+
 
 resource "azurerm_resource_group" "lab" {
   name     = "hyperv-nested-rg"
@@ -21,15 +57,52 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+# Public IP for each VM
+resource "azurerm_public_ip" "vm_public_ip" {
+  count               = 2
+  name                = "hyperv-pip-${count.index}"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Network Security Group
+resource "azurerm_network_security_group" "vm_nsg" {
+  name                = "hyperv-nsg"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "${local.current_ip}/32"  # Restricted to current public IP
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate NSG with subnet
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
+
 resource "azurerm_network_interface" "nic" {
   count               = 2
   name                = "hyperv-nic-${count.index}"
   location            = azurerm_resource_group.lab.location
   resource_group_name = azurerm_resource_group.lab.name
+  
   ip_configuration {
     name                          = "ipconfig"
-    subnet_id                    = azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip[count.index].id
   }
 }
 
@@ -39,8 +112,8 @@ resource "azurerm_windows_virtual_machine" "hyperv_node" {
   location            = azurerm_resource_group.lab.location
   resource_group_name = azurerm_resource_group.lab.name
   size                = "Standard_D4s_v3"
-  admin_username      = "labadmin"
-  admin_password      = "SuperSecurePassword123!"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
 
   network_interface_ids = [azurerm_network_interface.nic[count.index].id]
   enable_automatic_updates = false
@@ -54,24 +127,22 @@ resource "azurerm_windows_virtual_machine" "hyperv_node" {
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2022-datacenter"
+    sku       = "2025-datacenter"
     version   = "latest"
   }
 
-  boot_diagnostics {
-    enabled = true
-  }
+  boot_diagnostics {}
 
   additional_unattend_content {
     setting = "AutoLogon"
     content = <<CONTENT
 <AutoLogon>
   <Password>
-    <Value>SuperSecurePassword123!</Value>
+    <Value>${var.admin_password}</Value>
     <PlainText>true</PlainText>
   </Password>
   <Enabled>true</Enabled>
-  <Username>labadmin</Username>
+  <Username>${var.admin_username}</Username>
 </AutoLogon>
 CONTENT
   }
@@ -79,3 +150,33 @@ CONTENT
   custom_data = base64encode(file("bootstrap.ps1"))
 }
 
+# Outputs for easy access to connection information
+output "vm_public_ips" {
+  description = "Public IP addresses of the VMs"
+  value = {
+    for i in range(2) : "hyperv-node-${i}" => azurerm_public_ip.vm_public_ip[i].ip_address
+  }
+}
+
+output "vm_private_ips" {
+  description = "Private IP addresses of the VMs"
+  value = {
+    for i in range(2) : "hyperv-node-${i}" => azurerm_network_interface.nic[i].private_ip_address
+  }
+}
+
+output "rdp_connection_info" {
+  description = "RDP connection information"
+  value = {
+    for i in range(2) : "hyperv-node-${i}" => {
+      public_ip = azurerm_public_ip.vm_public_ip[i].ip_address
+      username  = var.admin_username
+      rdp_port  = 3389
+    }
+  }
+}
+
+output "allowed_source_ip" {
+  description = "Your current public IP that is allowed RDP access"
+  value       = local.current_ip
+}
