85dfeb7840bed6bd7de86ae4e27faeee17fb8215
serge.meeuwsen@gmail.com <serge.meeuwsen@gmail.com>
2025-08-16 16:19:43 +0100

    Updated to use FQDN to make it WORKGROUP safe

--- setup-https-winrm-credssp.ps1
+++ setup-https-winrm-credssp.ps1
@@ -1,3 +1,26 @@
+# setup winrm and credssp
+function Resolve-FQDN {
+    param ($Name)
+    try {
+        $fqdn = [System.Net.Dns]::GetHostEntry($Name).HostName
+        Write-Host "Resolved $Name → $fqdn"
+        return $fqdn
+    } catch {
+        Write-Warning "DNS resolution failed for $Name. Using original name."
+        return $Name
+    }
+}
+
 $nodes = @("hyperv-node-0", "hyperv-node-1")
-.\configure-credssp-winrm.ps1 -ClusterNodes $nodes
+
+$fqdnList = @()
+foreach ($name in $nodes) {
+    $fqdnList += Resolve-FQDN -Name $name
+}
+
+Write-Host "`nFinal FQDN list:"
+$fqdnList | ForEach-Object { Write-Host $_ }
+
+write-host "You will be prompted for credentials several times!" -ForegroundColor Cyan
+.\configure-credssp-winrm.ps1 -ClusterNodes $fqdnList
 
